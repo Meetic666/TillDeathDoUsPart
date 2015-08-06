26da52e4af1225310d9188516f0535f1c09e1887
Quentin Bellay <bellay.quentin@gmail.com>
2015-08-06 22:50:18 +0200

    Flying enemy is working (needs attacking stuff though)

--- Project/Assets/Scripts/Enemies/Generic/KnockbackState.cs
+++ Project/Assets/Scripts/Enemies/Generic/KnockbackState.cs
@@ -5,6 +5,8 @@ public class KnockbackState : InterruptionState
 {
 	public float m_DamageForceRatio;
 
+	float m_InitialHeight;
+
 	NavMeshAgent m_Agent;
 	Rigidbody m_Rigidbody;
 
@@ -16,25 +18,48 @@ public class KnockbackState : InterruptionState
 
 	public override void EnterState ()
 	{
-		m_Agent.enabled = false;
-		m_Rigidbody.isKinematic = false;
+		if(m_Agent)
+		{
+			m_Agent.enabled = false;
+		}
+
+		if(m_Rigidbody)
+		{
+			m_Rigidbody.isKinematic = false;
+		}
+
+		m_InitialHeight = transform.position.y;
 	}
 
 	public override bool CanExitState ()
 	{
-		return m_Rigidbody.velocity == Vector3.zero;
+		return (m_Rigidbody && (m_Rigidbody.velocity == Vector3.zero || (m_Rigidbody.velocity.y < 0.0f && transform.position.y <= m_InitialHeight)));
 	}
 
 	public override void ExitState ()
 	{
-		m_Agent.enabled = true;
-		m_Rigidbody.isKinematic = true;
+		Vector3 newPosition = transform.position;
+		newPosition.y = m_InitialHeight;
+		transform.position = newPosition;
+
+		if(m_Agent)
+		{
+			m_Agent.enabled = true;
+		}
+		
+		if(m_Rigidbody)
+		{
+			m_Rigidbody.isKinematic = true;
+		}
 	}
 
 	public void SetUpKnockback(Vector3 velocity, float damage)
 	{
 		velocity.y = 0.0f;
 
-		m_Rigidbody.AddForce(velocity + Vector3.up * damage * m_DamageForceRatio * 0.5f);
+		if(m_Rigidbody)
+		{
+			m_Rigidbody.AddForce(velocity + Vector3.up * damage * m_DamageForceRatio * 0.5f);
+		}
 	}
 }
--- Project/Assets/Scripts/Enemies/Minions/Flying Demon/FlyingState.cs
+++ Project/Assets/Scripts/Enemies/Minions/Flying Demon/FlyingState.cs
@@ -3,14 +3,51 @@ using System.Collections;
 
 public class FlyingState : MovementState
 {	
-	public float m_RotationSpeed;
+	Vector3 m_Shortcut;
+
+	NavMeshAgent m_Agent;
+
+	protected override void Start ()
+	{
+		base.Start ();
+		
+		m_Agent = GetComponent<NavMeshAgent>();
+		m_Agent.Stop ();
+	}
 
 	public override void UpdateState ()
 	{
 		base.UpdateState ();		
 		
 		Vector3 targetForward = (m_TargettedPlayer.transform.position - transform.position).normalized;
+		targetForward.y = 0.0f;
 		
-		transform.forward = Vector3.Slerp (transform.forward, targetForward, m_RotationSpeed * Time.deltaTime);
+		transform.forward = Vector3.Slerp (transform.forward, targetForward, m_Agent.angularSpeed * Mathf.Deg2Rad * Time.deltaTime);
+
+		m_Agent.SetDestination(m_TargettedPlayer.transform.position);
+
+		CheckForShortcut();
+
+		Vector3 displacement = m_Agent.speed * Time.deltaTime * (m_Shortcut - transform.position).normalized;
+
+		transform.position += displacement;
+		
+		m_Agent.updatePosition = false;
+	}
+
+	void CheckForShortcut()
+	{
+		foreach(Vector3 waypoint in m_Agent.path.corners)
+		{
+			Vector3 position = waypoint;
+			position.y = transform.position.y;
+
+			RaycastHit hitInfo;
+
+			if(!Physics.SphereCast (transform.position, m_Agent.radius, position - transform.position, out hitInfo))
+			{
+				m_Shortcut = position;
+			}
+		}
 	}
 }
