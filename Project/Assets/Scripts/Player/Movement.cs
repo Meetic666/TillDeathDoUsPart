01d38c93232cc052b5de0a9f592a10d3873b2b7a
Quentin Bellay <bellay.quentin@gmail.com>
2015-09-12 01:36:27 +0200

    Added colonnades to church level
    Added threshold for players to not be too far from each other

--- Project/Assets/Scripts/Player/Movement.cs
+++ Project/Assets/Scripts/Player/Movement.cs
@@ -5,10 +5,14 @@ public class Movement : GenericState
 {
 	public float m_Speed;
 
+	public float m_MaxDistanceFromPlayers;
+
 	PlayerInput m_Input;
 	NavMeshAgent m_Agent;
 
 	Interaction m_Interaction;
+
+	PlayerInput[] m_Players;
 
 	// Use this for initialization
 	protected override void StartVirtual ()
@@ -17,6 +21,8 @@ public class Movement : GenericState
 		m_Agent = GetComponent<NavMeshAgent>();
 
 		m_Interaction = GetComponent<Interaction>();
+
+		m_Players = FindObjectsOfType<PlayerInput>();
 	}
 
 	public override bool CanEnterState ()
@@ -33,14 +39,36 @@ public class Movement : GenericState
 			displacement = Vector3.zero;
 		}
 
-		NavMeshHit hitInfo;
-
-		if(m_Agent.Raycast(transform.position + displacement, out hitInfo))
+		if(displacement != Vector3.zero)
 		{
-			Vector3 obstacleTangent = Vector3.Cross (hitInfo.normal, Vector3.up);
-			float distanceAlongTangent = Vector3.Dot(obstacleTangent, displacement);
+			Vector3 playersCenter = CalculatePlayersCenter();
+			
+			if(Vector3.Distance(transform.position + displacement, playersCenter) > m_MaxDistanceFromPlayers)
+			{
+				Vector3 targetPosition = transform.position + displacement;
+				Vector3 vectorFromPlayers = targetPosition - playersCenter;
 
-			displacement = distanceAlongTangent * obstacleTangent;
+				vectorFromPlayers.Normalize ();
+
+				vectorFromPlayers *= m_MaxDistanceFromPlayers;
+
+				targetPosition = playersCenter + vectorFromPlayers;
+
+				displacement = targetPosition - transform.position;
+			}
+
+			if(displacement != Vector3.zero)
+			{
+				NavMeshHit hitInfo;
+				
+				if(m_Agent.Raycast(transform.position + displacement, out hitInfo))
+				{
+					Vector3 obstacleTangent = Vector3.Cross (hitInfo.normal, Vector3.up);
+					float distanceAlongTangent = Vector3.Dot(obstacleTangent, displacement);
+					
+					displacement = distanceAlongTangent * obstacleTangent;
+				}
+			}
 		}
 
 		m_Agent.Move (displacement);
@@ -55,5 +83,22 @@ public class Movement : GenericState
 	{
 		m_CharacterAnimator.SetFloat ("ForwardDotProduct", 0.0f);
 		m_CharacterAnimator.SetFloat ("RightDotProduct", 0.0f);
+	}
+
+	Vector3 CalculatePlayersCenter()
+	{
+		Vector3 playerCenter = Vector3.zero;
+		
+		for(int i = 0; i < m_Players.Length; i++)
+		{
+			if(m_Players[i] != m_Input)
+			{
+				playerCenter += m_Players[i].transform.position;
+			}	
+		}
+		
+		playerCenter /= (m_Players.Length - 1);
+
+		return playerCenter;
 	}
 }
